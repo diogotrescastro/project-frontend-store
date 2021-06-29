c0b555673096c7b5f26f4f40c8b971ae7d18f555
Diogo Augusto <diogotrescastro@gmail.com>
2021-06-28 22:57:26 -0300

    atualizaçao

---
+++ build/precache-manifest.9027d1687ee3c0704a1ce21c4a4560ed.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b3b79d9e1e1d7592a5f503f2282f2da",
+    "url": "/project-frontend-store/index.html"
+  },
+  {
+    "revision": "ff58b47fe8adeae1d31a",
+    "url": "/project-frontend-store/static/css/main.339ba6c5.chunk.css"
+  },
+  {
+    "revision": "54e06518931cb926a679",
+    "url": "/project-frontend-store/static/js/2.14c423b4.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/project-frontend-store/static/js/2.14c423b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff58b47fe8adeae1d31a",
+    "url": "/project-frontend-store/static/js/main.cc585163.chunk.js"
+  },
+  {
+    "revision": "bd9db756dbc28da442a9",
+    "url": "/project-frontend-store/static/js/runtime-main.c2843ee3.js"
+  },
+  {
+    "revision": "d4d545f164c544ba21528695dd9bc483",
+    "url": "/project-frontend-store/static/media/arrow_back.d4d545f1.svg"
+  },
+  {
+    "revision": "2396a4359fdbe8ece3e79739d18f2f4d",
+    "url": "/project-frontend-store/static/media/banner.2396a435.png"
+  },
+  {
+    "revision": "2c91ce7e84fd776c51d3edfddbec7d3c",
+    "url": "/project-frontend-store/static/media/cart.2c91ce7e.svg"
+  },
+  {
+    "revision": "7085893ab8e7b31f7a530a4c4d7e15e8",
+    "url": "/project-frontend-store/static/media/cart_empty.7085893a.svg"
+  },
+  {
+    "revision": "ded7f22d183b049dbfa8c9231419da33",
+    "url": "/project-frontend-store/static/media/freeship.ded7f22d.svg"
+  },
+  {
+    "revision": "6f4f1f61e00003cde8e7b7a251050aa3",
+    "url": "/project-frontend-store/static/media/git.6f4f1f61.svg"
+  },
+  {
+    "revision": "615d402a198740476781efd6b717f8ed",
+    "url": "/project-frontend-store/static/media/in.615d402a.svg"
+  },
+  {
+    "revision": "9a82486b850a0b6fcc110730616c8c5b",
+    "url": "/project-frontend-store/static/media/logo_frontend_store.9a82486b.svg"
+  },
+  {
+    "revision": "6cd35e6c15a5c1680d692cf8ff03646e",
+    "url": "/project-frontend-store/static/media/user.6cd35e6c.png"
+  }
+]);
